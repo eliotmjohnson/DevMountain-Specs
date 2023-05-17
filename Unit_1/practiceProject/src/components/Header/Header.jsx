8d9b768812ffdb871ba4bc536b00122d4b86640d
Eliot Johnson <eliotmjohnson@gmail.com>
2023-05-17 16:26:49 -0500

    Init Commit

--- Unit_1/practiceProject/src/components/Header/Header.jsx
+++ Unit_1/practiceProject/src/components/Header/Header.jsx
@@ -2,44 +2,37 @@ import { useState } from "react";
 import "./Header.css";
 import NavLinks from "./NavLinks";
 
-const Header = () => {
+const Header = (props) => {
 	const [headerClassList, setHeaderClassList] = useState("header");
-    const [headerTitleText, setHeaderTitleText] = useState("My Header");
-    const [headerWords, setHeaderWords] = useState("")
-	const [navText, setNavText] = useState(
-		<>
-			<a href="">Link</a>
-			<a href="">Link</a>
-			<a href="">Link</a>
-		</>
-	);
+	const [headerTitleText, setHeaderTitleText] = useState("Picture Gallery");
+	const [headerWords, setHeaderWords] = useState("");
 
 	const headerAnimation = () => {
 		if (headerClassList === "header") {
 			setHeaderClassList("header header-animation");
 			setHeaderTitleText("😡😡😡");
-            setNavText("😡😡😡");
-            setHeaderWords(<h1 id="lol">DON'T TOUCH ME!!</h1>)
+			props.setText("😡😡😡");
+			setHeaderWords(<h1 id="lol">DON'T TOUCH ME!!</h1>);
 			setTimeout(() => {
 				setHeaderClassList("header");
-				setHeaderTitleText("My Header");
-				setNavText(
+				setHeaderTitleText("Picture Gallery :)");
+				props.setText(
 					<>
 						<a href="">Link</a>
 						<a href="">Link</a>
 						<a href="">Link</a>
 					</>
-                );
-                setHeaderWords("");
-			}, 2000);
+				);
+				setHeaderWords("");
+			}, 1000);
 		}
 	};
 
 	return (
 		<header className={headerClassList} onClick={headerAnimation}>
-            <h1>{headerTitleText}</h1>
-            {headerWords}
-			<NavLinks onHeaderClick={navText} />
+			<h1>{headerTitleText}</h1>
+			{headerWords}
+			<NavLinks onHeaderClick={props.text} />
 		</header>
 	);
 };
---
+++ Unit_1/practiceProject/src/components/PictureBox/PictureBox.jsx
@@ -0,0 +1,23 @@
+import { useInView } from "react-hook-inview";
+import "./PictureBox.css";
+
+const PictureBox = (props) => {
+	const [ref, isVisible] = useInView({
+		threshold: 0.6,
+		unobserveOnEnter: false,
+	});
+
+	let classes = "before";
+
+	if (isVisible) {
+		classes = "before hello-world";
+	}
+
+	return (
+		<div ref={ref} className="box">
+			<img className={classes} src={props.url} />
+		</div>
+	);
+};
+
+export default PictureBox;
